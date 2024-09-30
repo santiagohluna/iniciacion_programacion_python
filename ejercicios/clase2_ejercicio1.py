7123751160883325189ee2268eca5310d3ec8113
Santiago Luna <santiagohluna@gmail.com>
2024-09-30 19:52:45 -0300

    Se creó el archivo con la resolución del ejercicio 1 de la clase 2

---
+++ ejercicios/clase2_ejercicio1.py
@@ -0,0 +1,16 @@
+"""
+Clase 2 - Ejercicio 1
+
+Lista del súper
+
+Consigna: Usando la instrucción print() y lo visto en la clase, escribir un programa que muestre en la terminal la lista de productos que necesitamos comprar en el supermercado.
+
+"""
+
+print("Lista del super:")
+print()
+print("\t1. Pan")
+print("\t2. Leche ")
+print("\t3. Queso")
+print("\t4. Manteca")
+print()
